b38f9fea7f98edab0a283421f4e7a6d4cf491f82
BarryDong <286084845@qq.com>
2019-06-04 15:10:48 +0800

    基于Cookie的Session

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ if(!port){
   process.exit(1)
 }
 
+let sessions = {
+
+}
+
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
   var pathWithQuery = request.url 
@@ -23,7 +27,10 @@ var server = http.createServer(function(request, response){
 
   if(path === '/'){
     let string = fs.readFileSync('./index.html', 'utf8')
-    let cookies = request.headers.cookie.split('; ')
+    let cookies = ''
+    if (request.headers.cookie) {
+      cookies = request.headers.cookie.split('; ')
+    }
     let hash = {}
     for (let i = 0; i < cookies.length; i ++) {
       let parts = cookies[i].split('=')
@@ -31,7 +38,11 @@ var server = http.createServer(function(request, response){
       let value = parts[1]
       hash[key] = value
     }
-    let email = hash.sign_in_email
+    let mySession = sessions[hash.sessionId]
+    let email
+    if (mySession) {
+      email = mySession.sign_in_email
+    }
     let users = fs.readFileSync('./db/users', 'utf8')
     users = JSON.parse(users)
     let foundUser
@@ -139,7 +150,9 @@ var server = http.createServer(function(request, response){
           }
         }
         if (found) {
-          response.setHeader('Set-Cookie', `sign_in_email=${email}; HttpOnly`)
+          let sessionId = Math.random() * 100000
+          sessions[sessionId] = {sign_in_email: email}
+          response.setHeader('Set-Cookie', `sessionId=${sessionId}; HttpOnly`)
           response.statusCode = 200
         } else {
           response.statusCode = 401
