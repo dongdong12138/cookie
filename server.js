24a47af79b69366bf82ddfddfa89a85c20fd0ebc
BarryDong <286084845@qq.com>
2019-06-04 23:40:02 +0800

    add Cache-Control

--- server.js
+++ server.js
@@ -25,7 +25,19 @@ var server = http.createServer(function(request, response){
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path === '/'){
+  if (path === '/js/main.js') {
+    let string = fs.readFileSync('./js/main.js', 'utf8')
+    response.setHeader('Content-Type', 'application/javascript;charset=utf-8')
+    response.setHeader('Cache-Control', 'max-age=30')
+    response.write(string)
+    response.end()
+  } else if (path === '/css/default.css') {
+    let string = fs.readFileSync('./css/default.css', 'utf8')
+    response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    response.setHeader('Cache-Control', 'max-age=30')
+    response.write(string)
+    response.end()  
+  } else if(path === '/'){
     let string = fs.readFileSync('./index.html', 'utf8')
     let cookies = ''
     if (request.headers.cookie) {
